84775805d41e57f21110fc7debd2e62cefe473ce
vladiukdaria <vladiukdaria@gmail.com>
2026-04-01 19:34:32 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -28,6 +28,17 @@ class BackendDeveloper(SoftwareEngineer):
         super().__init__(name)
         self.skills = ["Java", "Android studio"]
 
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills = ["Java", "Android studio"]
+        self.skills.extend(self.skills)
+
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
