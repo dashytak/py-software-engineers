6edb9dfeb2afc78a6def4815f4b369f7ef657a0e
vladiukdaria <vladiukdaria@gmail.com>
2026-04-01 19:22:11 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,46 @@
 # write your code here
+class SoftwareEngineer:
+    def __init__(
+        self,
+        name: str,
+    ) -> None:
+        self.name = name
+        self.skills = []
+
+    def learn_skills(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(
+        self,
+        name: str,
+    ) -> None:
+        super().__init__(name)
+        self.skills = ["JavaScript", "HTML", "CSS"]
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(
+        self,
+        name: str,
+    ) -> None:
+        super().__init__(name)
+        self.skills = ["Java", "Android studio"]
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    def __init__(
+        self,
+        name: str,
+    ) -> None:
+        super().__init__(name)
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
